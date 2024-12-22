be4410ac5e29465640f07da7a508c37dc9354009
purewack <bobimaster15@gmail.com>
2024-12-22 13:30:45 +0000

    esp32: Customise ULP module inclusion.
    Add the ability to disable the built in ULP module
    using mpconfigport.h
    Add the ability to compile custom ULP image into
    the build by defining the cmake variables:
        ulp_app_name
        ulp_sources
        ulp_exp_dep_srcs
    
    Signed-off-by: Damian Nowacki (purewack) <bobimaster15@gmail.com>

--- ports/esp32/modesp32.c
+++ ports/esp32/modesp32.c
@@ -305,7 +305,7 @@ static const mp_rom_map_elem_t esp32_module_globals_table[] = {
     #if SOC_RMT_SUPPORTED
     { MP_ROM_QSTR(MP_QSTR_RMT), MP_ROM_PTR(&esp32_rmt_type) },
     #endif
-    #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+    #if MICROPY_PY_ULP && (CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3)
     { MP_ROM_QSTR(MP_QSTR_ULP), MP_ROM_PTR(&esp32_ulp_type) },
     #endif
 
